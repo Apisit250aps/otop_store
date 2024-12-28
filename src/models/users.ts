43ec5b128ab386d3ed13309fed8fdeda12ff5afa
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-12-28 19:55:29 +0700

    users model

---
+++ src/models/users.ts
@@ -0,0 +1,37 @@
+import { ObjectId, Schema, Document, model } from "mongoose"
+import bcrypt from "bcrypt"
+
+export interface IUser extends Document {
+  id: ObjectId
+  name: string
+  email: string
+  password: string
+  authentication(password: string): Promise<boolean>
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+  },
+  { timestamps: true }
+)
+
+userSchema.pre("save", async function (next) {
+  const user = this
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 10)
+  }
+  next()
+})
+
+userSchema.methods.authentication = async function (
+  password: string
+): Promise<boolean> {
+  return await bcrypt.compare(password, this.password)
+}
+
+const User = model<IUser>("User", userSchema)
+
+export default User
